b628d655c708a977a4b561e06d267c93b5e1802d
Jorge Morayta <jmorayta@tdsi.com>
2019-09-11 17:11:15 -0500

    Adding Clarity dependencies

--- tds/libs/tds-component-library/src/lib/tds-component-library.module.ts
+++ tds/libs/tds-component-library/src/lib/tds-component-library.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {ClarityModule} from '@clr/angular';
 
 @NgModule({
-	imports: [CommonModule, RouterModule],
-	declarations: [
+	imports: [
+		CommonModule,
+		RouterModule,
+		ClarityModule
 	],
-	exports: [
-	]
+	declarations: [],
+	exports: []
 })
-export class TdsComponentLibraryModule {}
+export class TdsComponentLibraryModule {
+}
