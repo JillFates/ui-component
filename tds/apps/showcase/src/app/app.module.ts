d4949b797ae2d17a1eac5ebb7449628d6c242329
Jorge Morayta <jmorayta@tdsi.com>
2019-09-12 10:43:59 -0500

    Added clarity module to the showcase app and initial empty nav

--- tds/apps/showcase/src/app/app.module.ts
+++ tds/apps/showcase/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import {ClarityModule} from '@clr/angular';
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [
 		BrowserModule,
+		ClarityModule,
 		RouterModule.forRoot([], { initialNavigation: 'enabled' })
 	],
 	providers: [],
