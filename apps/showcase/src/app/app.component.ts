39dca141dc91bb06196edf1ef1906d80cce1cabe
Jorge Morayta <jmorayta@tdsi.com>
2020-01-31 15:58:53 -0600

    TM-16843 - Refactoring some CSS Bases

--- apps/showcase/src/app/app.component.ts
+++ apps/showcase/src/app/app.component.ts
@@ -9,8 +9,7 @@ import {SECTIONS} from './model/tab-section.model';
 
 @Component({
 	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+	templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
 
