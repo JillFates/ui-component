40b137bf37f5462b0db95a1d23e43e3a2f29ad1d
Jorge Morayta <jmorayta@tdsi.com>
2020-02-26 10:58:15 -0600

    Adding new dialog type, notification

---
+++ apps/showcase/src/app/modules/dialog-notify/component/api/dialog-notify-api.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-dialog-notify-api',
+	templateUrl: './dialog-notify-api.component.html',
+	styleUrls: ['./dialog-notify-api.component.scss'],
+})
+export class DialogNotifyApiComponent {}
