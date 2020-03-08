e5aedecb750a4df04863792ee7520c697158ab19
Jorge Morayta <jmorayta@tdsi.com>
2020-03-08 13:49:01 -0600

    Fix Buttons not showing after refresh Fix blinking Dialog Background Support HTML Title

--- libs/tds-component-library/src/lib/dialog/component/dynamic-host/dynamic-host.component.ts
+++ libs/tds-component-library/src/lib/dialog/component/dynamic-host/dynamic-host.component.ts
@@ -54,28 +54,38 @@ export class DynamicHostComponent implements OnInit {
 			} else {
 				// Set the Title
 				this.currentDialogComponentInstance.setTitle(this.modalConfigurationModel.title);
-
-				const actionButtons = this.currentDialogComponentInstance.buttons.filter((button: DialogButtonModel) => {
-					return button.type === DialogButtonType.ACTION;
-				});
-				const contextButtons = this.currentDialogComponentInstance.buttons.filter((button: DialogButtonModel) => {
-					return button.type === DialogButtonType.CONTEXT;
-				});
-
-				if (!actionButtons || actionButtons.length === 0) {
-					this.showLeftActionButtonsPanel = false;
-				} else if (actionButtons && actionButtons.length > 0) {
-					this.showLeftActionButtonsPanel = this.showActionButtons = true;
-				}
-
-				this.showContextButtons = (contextButtons && contextButtons.length > 0);
+				// Initial Buttons
+				this.publishButtons();
 
 				if (this.modalConfigurationModel.fullScreen || this.modalConfigurationModel.defaultFullScreen) {
 					this.showFullScreen = true;
 					this.fullScreen = this.modalConfigurationModel.defaultFullScreen;
 				}
+
+				// Listen to any change
+				setInterval( () => this.publishButtons(), 500);
 			}
 		});
+	}
+
+	/**
+	 * Verify if new actions are coming to the Instance, to show Hide Sections
+	 */
+	public publishButtons(): void {
+		const actionButtons = this.currentDialogComponentInstance.buttons.filter((button: DialogButtonModel) => {
+			return button.type === DialogButtonType.ACTION;
+		});
+		const contextButtons = this.currentDialogComponentInstance.buttons.filter((button: DialogButtonModel) => {
+			return button.type === DialogButtonType.CONTEXT;
+		});
+
+		if (!actionButtons || actionButtons.length === 0) {
+			this.showLeftActionButtonsPanel = false;
+		} else if (actionButtons && actionButtons.length > 0) {
+			this.showLeftActionButtonsPanel = this.showActionButtons = true;
+		}
+
+		this.showContextButtons = (contextButtons && contextButtons.length > 0);
 	}
 
 	/**
