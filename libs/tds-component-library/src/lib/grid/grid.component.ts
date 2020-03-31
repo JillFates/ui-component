ed454fdbf238a9d9f32c99103dc02c7715a0c034
Hector Coss <hcoss@tdsi.com>
2020-03-30 22:46:15 -0600

    TM-16127 Grid component - bind pageSize to state

--- libs/tds-component-library/src/lib/grid/grid.component.ts
+++ libs/tds-component-library/src/lib/grid/grid.component.ts
@@ -52,6 +52,7 @@ export class GridComponent implements OnInit {
 		// Next, we pass the grid data to the data-grid-helper.
 		this.gridHelper = new DataGridHelper(gridData, this.gridModel.gridSettings);
 
+		this.gridHelper.state.take = this.gridModel.gridSettings.pageSize || 25;
 	}
 
 	/**
