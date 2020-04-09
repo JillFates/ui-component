a7a4022bf14df3af564e705ef8e3cab32ac8be21
Sam Zuniga <szuniga@tdsi.com>
2020-04-09 12:18:27 -0500

    TM-17465 Add comment

--- libs/tds-component-library/src/lib/grid/grid.component.ts
+++ libs/tds-component-library/src/lib/grid/grid.component.ts
@@ -54,6 +54,7 @@ export class GridComponent implements OnInit {
 
 		// Initialize the loading message
 		this.gridModel.gridSettings.loadingMessage = this.gridModel.gridSettings.loadingMessage || 'Loading records...';
+		// Initialize the no records available message
 		this.gridModel.gridSettings.noRecordsMessage = this.gridModel.gridSettings.noRecordsMessage || 'No records available';
 	}
 
